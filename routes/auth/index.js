bfe8413768ec17619855aeaca70205cbd0ff0830
Jeevu <jeevu@Jeevus-MacBook-Air.local>
2024-05-20 20:33:40 +0530

    Testing husky attempt 4

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -17,7 +17,8 @@ router.get("/login", (req, res) => {
     if (!username || !password) {
       throw new Error("Invalid input :(");
     }
-    const user = JSON.parse(fs.readFileSync("userdata.json", "utf8"));
+    const user = JSON.parse(fs.readFileSync("userdata.json", "utf8"))
+    ;
     if (userData.username == username && userData.password == password) {
       console.log("User succesfully logged in ;)");
       const token = jwt.sign(userData, process.env.SECRET_KEY, {
